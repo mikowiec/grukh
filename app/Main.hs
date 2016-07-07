afb719d1ece97f9e85989fe332366eef60ec5eef
Mikolay <mikki@Mikolays-MacBook-Pro.local>
2016-07-07 17:47:45 +0300

    add aeson, attoparsec

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,62 @@
+-- {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+
 module Main where
 
+import Data.Aeson
+import qualified Data.Aeson.Types as T
 import Lib
+--import Options.Applicative
+--import Control.Monad
+import Data.Text
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy.Char8 as BSL
+import Control.Monad     --(mzero)
+import Data.Attoparsec (parse, Result(..))
+import Data.Attoparsec.ByteString
+import Control.Applicative ((<$>))
+
+
+-- Types
+data Msg = Msg Text deriving (Show)
+
+-- parse JSON and Msg constructor
+instance FromJSON Msg where
+  parseJSON (Object v) = Msg <$> v .: "message"
+  parseJSON _ = mzero
+
+instance ToJSON Msg where
+  toJSON (Msg s) = object [ "message" .= s]
+
+
+-- {"message": "hello world"}
+exampleJSONMessage :: String
+exampleJSONMessage = "{\"message\":\"hello world\"}"
+
+
+parseMsg :: String -> Maybe Msg
+parseMsg s = let bs = BS.pack s
+             in case parse json bs of
+                  (Done rest r) -> T.parseMaybe parseJSON r :: Maybe Msg
+                  _             -> Nothing
+
+
 
 main :: IO ()
-main = someFunc
+main = do
+  print $ parseMsg exampleJSONMessage
+  let reply = Msg "It's Aeson!"
+  putStrLn $ "Encoded: " ++ (BSL.unpack (encode reply))
+  grokhMain
+
+-- main = do
+-- execcParser (info parser fullDesc)
+--  (uncurry cmdExec)
+
+
+
+
+
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,53 @@
 module Lib
-    ( someFunc
+    ( grokhMain
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import System.Environment
+
+grokhMain :: IO ()
+grokhMain = do
+   putStrLn "someFunc"
+   (command:argList) <- getArgs
+   dispatch command argList
+
+
+dispatch :: String -> [String] -> IO ()
+dispatch "start" = cmdStart                      -- start tunnels by name from config file
+dispatch "startAll" = cmdStartAll                -- start all tunnels defined in config file
+dispatch "list" = cmdList                        -- list tunnel names from config file
+dispatch "help" = cmdHelp                        -- view help
+dispatch "version" = cmdVersion                  -- view version
+
+
+cmdStart :: [String] -> IO ()
+cmdStart [opts] = do
+  putStrLn opts
+  putStrLn "cmd"
+
+cmdStartAll :: [String] -> IO ()
+cmdStartAll [opts] = do
+  putStrLn opts
+  putStrLn "cmd"
+
+cmdList :: [String] -> IO ()
+cmdList [opts] = do
+  putStrLn opts
+  putStrLn "cmd"
+
+cmdHelp :: [String] -> IO ()
+cmdHelp [opts] = do
+  putStrLn opts
+  putStrLn "cmd"
+
+cmdVersion :: [String] -> IO ()
+cmdVersion [opts] = do
+  putStrLn opts
+  putStrLn "cmd"
+
+
+
+
+
+
+
+
